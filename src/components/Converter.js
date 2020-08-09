738e7e39bb2b9b780ae55466ba8ba7c45c25192f
Schlenges <meike_hankewicz@yahoo.de>
2020-08-09 14:34:14 +0200

    add all basic logic

--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,15 +1,8 @@
 import React, { useState } from 'react'
 
-  // g * 0.035274
-  // oz / 0.035274
-
-  // cm * 0.39370
-  // in / 0.39370
-
 const Converter = () => {
-
-  const [unitA, setUnitA] = useState("Kilogram")
-  const [unitB, setUnitB] = useState("Fahrenheit")
+  const [unitA, setUnitA] = useState({unit: "Celsius", symbol: "°C"})
+  const [unitB, setUnitB] = useState({unit: "Fahrenheit", symbol: "°F"})
   const [measurement, setMeasurement] = useState("temperature")
   const [input, setInput] = useState("")
   const [output, setOutput] = useState("")
@@ -17,13 +10,13 @@ const Converter = () => {
   const _convert = (measurement) => {
     switch (measurement) {
       case "temperature":
-        let temp = unitA === "Celsius" ? input * 1.8000 + 32.00 : (input - 32) / 1.8000
+        let temp = unitA.unit === "Celsius" ? input * 1.8000 + 32.00 : (input - 32) / 1.8000
         return temp.toFixed(1)
       case "weight":
-        let weight = unitA === "Kilogram" ? input * 2.2046 : input / 2.2046
+        let weight = unitA.unit === "Kilogram" ? input * 2.2046 : input / 2.2046
         return weight.toFixed(1)
       case "length":
-        let length = unitA === "Meters" ? input * 3.2808 : input / 3.2808
+        let length = unitA.unit === "Meters" ? input * 3.2808 : input / 3.2808
         return length.toFixed(2)
     }
   }
@@ -31,26 +24,31 @@ const Converter = () => {
   const onSubmit = (event) => {
     event.preventDefault()
     setOutput(_convert(measurement))
-    // clear state
   }
 
-  const onChange = (value) => {
+  const onSelectChange = (value) => {
     switch (value) {
       case "temperature":
-        setUnitA("Celsius")
-        setUnitB("Fahrenheit")
+        setUnitA({unit: "Celsius", symbol: "°C"})
+        setUnitB({unit: "Fahrenheit", symbol: "°F"})
         break
       case "weight":
-        setUnitA("Kilogram")
-        setUnitB("Pound")
+        setUnitA({unit: "Kilogram", symbol: "kg"})
+        setUnitB({unit: "Pound", symbol: "lbs"})
         break
       case "length":
-        setUnitA("Meters")
-        setUnitB("Feet")
+        setUnitA({unit: "Meters", symbol: "m"})
+        setUnitB({unit: "Feet", symbol: "ft"})
         break
     }
 
     setMeasurement(value)
+  }
+
+  const onSwitch = () => {
+    setUnitA(unitB)
+    setUnitB(unitA)
+    // clear form after switch?
   }
 
   return (
@@ -60,34 +58,41 @@ const Converter = () => {
         <select 
           name="measurements"
           id="measurements" 
-          onChange={({target}) => onChange(target.value)}
+          onChange={({target}) => onSelectChange(target.value)}
         >
           <option value="temperature">Temperature</option>
           <option value="weight">Weight</option>
           <option value="length">Length</option>
         </select>
 
-        <p>Convert from {unitA} to {unitB} <button>switch</button></p>
+        <p>Convert from {unitA.unit} to {unitB.unit} 
+        <button onClick={() => onSwitch()}>switch</button></p>
         <input 
           type="text"
           id="input"
           value={input}
-          placeholder={unitA}
+          placeholder={unitA.unit}
           onChange={({target}) => setInput(target.value)}
         />
-         ➜ 
+          {unitA.symbol} ➜ 
         <input
           type="text"
           id="output"
           value={output}
-          placeholder={unitB}
+          placeholder={unitB.unit}
           readOnly
-        />
+        /> {unitB.symbol}
         <br></br>
         <button>convert</button>
       </form>
     </div>
   )
 }
+
+// g * 0.035274
+// oz / 0.035274
+
+// cm * 0.39370
+// in / 0.39370
 
 export default Converter
