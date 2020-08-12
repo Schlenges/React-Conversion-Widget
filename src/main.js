3b8b8cf8d3fd37d971b542e76bc26b1de6c306bb
Schlenges <meike_hankewicz@yahoo.de>
2020-08-12 03:22:17 +0200

    add electron

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 510, height: 370});
+    mainWindow.loadURL('http://localhost:3000');
+    // mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
